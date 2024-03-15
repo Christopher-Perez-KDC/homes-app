778440eb1c2af4b5c9b26cb1d657fb784d8a8b3e
Christopher Perez <christopher.perez19kdc@gmail.com>
2024-03-15 14:03:32 -0500

    A lot of progress, images and links working

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { Provider } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import routeConfig from './app/routes';
 bootstrapApplication(AppComponent, {
 
   providers:[
-    provideRouter([])
+    provideRouter(routeConfig)
   ]
 })
   .catch(err => console.error(err));
